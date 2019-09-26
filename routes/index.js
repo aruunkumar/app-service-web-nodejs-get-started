b4f9eec7a777d7e554bed2b86bac88f2c4b6d7aa
Aruun Kumar <aruunkumar@gmail.com>
2019-09-25 23:23:15 -0400

    changed title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Aruun's Express App on Azure' });
+  res.render('index', { title: 'Express App on Azure by Aruun' });
 });
 
 module.exports = router;
